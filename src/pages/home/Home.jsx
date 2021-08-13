a2c6b84541d118ebd834b3a5ad33e27b71251f1d
Omar Henawy <omarhenawy31@gmail.com>
2021-08-13 04:24:58 +0200

    test

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { AcUnit } from '@material-ui/icons'
+import "./home.scss"
+import Navbar from '../../components/Navbar'
+import { Featured } from '../../components/featured/Featured';
+import { List } from '../../components/list/List';
+const Home = () => {
+    return (
+        <div className="home">
+  <Navbar/>
+ 
+          <Featured />
+          <List/>
+          <List/>
+          <List/>
+          <List/>
+          <List/>
+          <List/>
+          <List/>
+          <List/>
+          <List/>
+          
+        </div>
+    )
+}
+
+export default Home
